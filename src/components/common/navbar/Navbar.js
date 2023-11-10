48dc51f5a9cb4e660d610760dd9b1523d184946a
mahmudul474 <mdmahmudulla474@gmail.com>
2023-11-10 21:00:36 +0600

    logo add

--- src/components/common/navbar/Navbar.js
+++ src/components/common/navbar/Navbar.js
@@ -1,20 +1,23 @@
- // components/common/Navbar.js
+import Image from "next/image";
+import  logo from "../../../assates/logo.png";
 
 import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-slate-300 p-4 list-none">
       <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
-          <li className="text-white text-2xl font-bold">Your Logo</li>
+          <li className="text-black  text-2xl font-bold">
+            <Image width={100}  height={200} className="h-[70px] object-cover w-full" src={logo} alt="Logo"/>
+          </li>
         </Link>
 
-        <div className="lg:hidden">
+        <div className="lg:hidden ">
           {/* Hamburger Icon for Mobile View */}
           <button
             type="button"
-            className="text-white focus:outline-none"
+            className="text-black  focus:outline-none"
             aria-label="Toggle navigation"
           >
             <svg
@@ -36,28 +39,28 @@ const Navbar = () => {
 
         <div className="hidden lg:flex space-x-4">
           <Link href="/pc-builder">
-            <li className="text-white">PC Builder</li>
+            <li className="text-black ">PC Builder</li>
           </Link>
           <Link href="/category/cpu-processor">
-            <li className="text-white">CPU / Processor</li>
+            <li className="text-black ">CPU / Processor</li>
           </Link>
           <Link href="/category/motherboard">
-            <li className="text-white">Motherboard</li>
+            <li className="text-black ">Motherboard</li>
           </Link>
           <Link href="/category/ram">
-            <li className="text-white">RAM</li>
+            <li className="text-black ">RAM</li>
           </Link>
           <Link href="/category/power-supply-unit">
-            <li className="text-white">Power Supply Unit</li>
+            <li className="text-black ">Power Supply Unit</li>
           </Link>
           <Link href="/category/storage-device">
-            <loi className="text-white">Storage Device</loi>
+            <loi className="text-black ">Storage Device</loi>
           </Link>
           <Link href="/category/monitor">
-            <li className="text-white">Monitor</li>
+            <li className="text-black ">Monitor</li>
           </Link>
           <Link href="/category/others">
-            <li className="text-white">Others</li>
+            <li className="text-black ">Others</li>
           </Link>
         </div>
       </div>
